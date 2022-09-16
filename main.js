692b872b496b237e70d1e79d3fc5c7226ea5287c
Diyasanthosh97 <91463964+Diyasanthosh97@users.noreply.github.com>
2022-09-16 19:44:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function liv(){
+    window.location="living_room.html";
+}
+function bed(){
+    window.location="bed.html";
+}
+function din(){
+    window.location="dine_room.html";
+}
+function kitchen(){
+    window.location="kitchen.html";
+}
